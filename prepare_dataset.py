878f1d9f8b777be6720b72dd7d12332a09c2cfbb
san999p <57917118+san999p@users.noreply.github.com>
2020-02-10 12:44:03 -0600

    create dataset with model and classes file

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -2,7 +2,6 @@ import json
 import csv
 import os
 
-
 # create csv file for inference
 def generate_csv(input_file):
 	with open(input_file) as f:
@@ -20,3 +19,18 @@ def generate_csv(input_file):
 if __name__ == '__main__':
 	import sys
 	generate_csv(sys.argv[1])
+
+	from datetime import datetime
+	time = datetime.now()
+	stamp = time.strftime("%m%d%Y%H%M%S")
+	dataset_name = "MaskRCNN-model-output-{}".format(stamp)
+	
+	os.system("onepanel datasets create {}".format(dataset_name))
+	os.chdir("/onepanel/code/{}".format(dataset_name))
+	os.system("mv /onepanel/output/classes.csv /onepanel/code/{}".format(dataset_name))
+	for i,_,_ in os.walk("/onepanel/output/logs"):
+		if "cvat" in i:
+			model_path = i
+	os.system("mv model_path/{} /onepanel/code/{}".format('mask_rcnn_cvat_0001.h5','dataset_name'))
+	print("\n\n\n")
+	print("Dataset Created with Name: {}".format(dataset_name))
