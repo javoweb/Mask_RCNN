ead4d00c136097b278b3fcec8dac25d7141c1ad6
san999p <57917118+san999p@users.noreply.github.com>
2020-02-10 12:20:15 -0600

    updated csv generator

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -2,14 +2,21 @@ import json
 import csv
 import os
 
+
 # create csv file for inference
-with open("annotations.json") as f:
-	data = json.load(f)
+def generate_csv(input_file):
+	with open(input_file) as f:
+		data = json.load(f)
 
-csv_out = open(os.path.join("/onepanel/output/", "classes.csv"), "w", newline='')
+	csv_out = open(os.path.join("/onepanel/output/", "classes.csv"), "w", newline='')
 
-csv_writer = csv.writer(csv_out)
-csv_writer.writerow(['labels','id'])
+	csv_writer = csv.writer(csv_out)
+	csv_writer.writerow(['labels','id'])
 
-for lbl in data['categories']:
-	csv_writer.writerow([lbl['name'], lbl['id']])
+	for lbl in data['categories']:
+		csv_writer.writerow([lbl['name'], lbl['id']])
+
+
+if __name__ == '__main__':
+	import sys
+	generate_csv(sys.argv[1])
