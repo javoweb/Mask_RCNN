a29cbb2fcdd8053b301b391db9676232192e430d
Savan Visalpara <savan77@users.noreply.github.com>
2020-07-01 16:34:14 -0500

    use model from models/

--- samples/coco/cvat.py
+++ samples/coco/cvat.py
@@ -504,15 +504,17 @@ if __name__ == '__main__':
             s3_resource = boto3.resource('s3')
             bucket = s3_resource.Bucket(os.getenv('AWS_BUCKET_NAME')) 
             for object in bucket.objects.filter(Prefix = args.ref_model_path):
+                print(os.path.basename(object.key))
                 bucket.download_file(object.key,'/mnt/data/models/'+os.path.basename(object.key))
             #find cvat dir
-            for path,_,_ in os.walk("/mnt/data/models/"):
-                if "cvat" in path.lower():
-                    cvat_path = path
-            if not cvat_path.endswith("/"):
-                cvat_path += "/"
+            # for path,_,_ in os.walk("/mnt/data/models/"):
+            #     print(path)
+            #     if "cvat" in path.lower():
+            #         cvat_path = path
+            # if not cvat_path.endswith("/"):
+            #     cvat_path += "/"
             # find last saved model
-            model_path = max(glob.glob(cvat_path+"mask_rcnn*"), key=os.path.getctime)
+            model_path = max(glob.glob("/mnt/data/models/"+"mask_rcnn*"), key=os.path.getctime)
             # except:
             #     print("Something went wrong while trying to find model from ref model path, using default model.")
             #     urllib.request.urlretrieve("https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5","/mnt/data/models/mask_rcnn_coco.h5")
